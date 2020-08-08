82cff4e063feeb77c47a6a6c8a993173212f5b39
luan11 <luaannovais@live.com>
2020-08-08 12:33:45 -0300

    Refactor: paginate method

--- src/SubmissionLoggerDao.php
+++ src/SubmissionLoggerDao.php
@@ -16,10 +16,8 @@ class SubmissionLoggerDao {
 	{
 		$logs = [];
 
-		$query = 'SELECT data, date FROM sl_logs ORDER BY datetime(date) DESC';
-		
-		$stmt = $this->database->prepare($query);
-		
+		$query = 'SELECT data, date FROM sl_logs ORDER BY datetime(date) DESC';		
+		$stmt = $this->database->prepare($query);		
 		$result = $stmt->execute();
 
 		while($log = $result->fetchArray(SQLITE3_ASSOC)) {
@@ -32,21 +30,25 @@ class SubmissionLoggerDao {
 		return $logs;
 	}
 
+	private function totalOfPages($perPage)
+	{
+		$query = 'SELECT count(*) FROM sl_logs';
+		$stmt = $this->database->prepare($query);
+		$result = $stmt->execute();
+
+		$total = $result->fetchArray(SQLITE3_NUM)[0];
+
+		return ceil($total / $perPage);
+	}
+
 	public function paginate($perPage = 10)
 	{
 		$currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-
 		$offset = ($currentPage - 1) * $perPage;
-
-		$queryTotal = 'SELECT count(*) FROM sl_logs';
-		
-		$stmtTotal = $this->database->prepare($queryTotal);		
-		$resultTotal = $stmtTotal->execute();
-		$total = $resultTotal->fetchArray(SQLITE3_NUM)[0];
-
-		$totalOfPages = ceil($total / $perPage);
+		$totalOfPages = $this->totalOfPages($perPage);
 
 		$logs = [];
+		
 		$query = 'SELECT data, date FROM sl_logs ORDER BY datetime(date) DESC LIMIT :offset, :perPage';
 		$stmt = $this->database->prepare($query);
 		$stmt->bindValue(':offset', $offset);
@@ -60,29 +62,9 @@ class SubmissionLoggerDao {
 			]);
 		}
 
-		$paginationOpen = '<ul class="pagination">';
-		$paginationClose = '</ul>';
-		$paginationItems = '';
-
-		if($currentPage > 1) {
-			$paginationItems .= '<li class="page-item"> <a class="page-link" href="?page='. ($currentPage - 1) .'" aria-label="Previous"> <span aria-hidden="true">&laquo;</span> </a> </li>';
-		}
-
-		for($i = 1; $i <= $totalOfPages; $i++) {
-			if($i === $currentPage) {
-				$paginationItems .= '<li class="page-item active"> <span class="page-link"> '. $i .' </span> </li>';
-			} else {
-				$paginationItems .= '<li class="page-item"><a class="page-link" href="?page='. $i .'">'. $i .'</a></li>';
-			}
-		}
-
-		if($currentPage < $totalOfPages) {
-			$paginationItems .= '<li class="page-item"> <a class="page-link" href="?page='. ($currentPage + 1) .'" aria-label="Next"> <span aria-hidden="true">&raquo;</span> </a> </li>';
-		}
-
 		return [
 			'logs' => $logs,
-			'pagination' => $paginationOpen . $paginationItems . $paginationClose
+			'pagination' => View::pagination($currentPage, $totalOfPages)
 		];
 	}
 
--- src/View.php
+++ src/View.php
@@ -12,4 +12,28 @@ class View {
 
 		include self::VIEWS_DIR . $name . self::VIEWS_SUFFIX;
 	}
+
+	public static function pagination($currentPage, $total)
+	{
+		$list = '<ul class="pagination">%s</ul>';
+		$lisItems = '';
+
+		if($currentPage > 1) {
+			$lisItems .= '<li class="page-item"> <a class="page-link" href="?page='. ($currentPage - 1) .'" aria-label="Previous"> <span aria-hidden="true">&laquo;</span> </a> </li>';
+		}
+
+		for($i = 1; $i <= $total; $i++) {
+			if($i === $currentPage) {
+				$lisItems .= '<li class="page-item active"> <span class="page-link"> '. $i .' </span> </li>';
+			} else {
+				$lisItems .= '<li class="page-item"><a class="page-link" href="?page='. $i .'">'. $i .'</a></li>';
+			}
+		}
+
+		if($currentPage < $total) {
+			$lisItems .= '<li class="page-item"> <a class="page-link" href="?page='. ($currentPage + 1) .'" aria-label="Next"> <span aria-hidden="true">&raquo;</span> </a> </li>';
+		}
+
+		return sprintf($list, $lisItems);
+	}
 }
